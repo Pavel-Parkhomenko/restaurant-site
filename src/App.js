8d4e95df55f0547be152e14ea0722804c1111697
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-14 13:28:33 +0300

    add containers and home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import { HomePage } from './pages';
 import './App.css';
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path='/' element={<h1>Hi</h1>} />
+      <Route path='/' element={<HomePage />} />
   </Routes>
 </Router>
 );
