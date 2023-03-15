761188934401925e4f525dcead51b1baa9e176d2
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-15 15:41:29 +0300

    add register page and keyframes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { HomePage, LoginPage } from './pages';
+import { HomePage, LoginPage, RegisterPage } from './pages';
 import './App.css';
 
 const App = () => (
@@ -9,6 +9,7 @@ const App = () => (
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
   </Routes>
 </Router>
 );
